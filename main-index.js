95dad2aa5a3a2a517a1c882cbec81836582fcc94
Artis Machinae <111511534+artis-machinae@users.noreply.github.com>
2023-02-15 14:57:19 -0500

    Add files via upload

---
+++ main-index.js
@@ -0,0 +1,30 @@
+// Scroll up button
+let scrollButton = document.getElementById("topButton");
+
+window.onscroll = function() {scrollUp()};
+
+function scrollUp() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 200) {
+    scrollButton.style.display = "block";
+    } else {
+    scrollButton.style.display = "none";
+    }
+};
+
+function scrollToTop() { 
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+// anch adjustment
+
+let navBarHeight = document.querySelector('.nav').offsetHeight;
+
+document.documentElement.style.setProperty('--wrapperTopMargin', `${navBarHeight + (navBarHeight * 0.75)}px`);
+
+function stickyHeightUpdate() {
+    document.documentElement.style.setProperty('--scrollAnch-padding', `${navBarHeight + 10}px`);
+};
+
+window.addEventListener("load", stickyHeightUpdate);
+window.addEventListener("resize", stickyHeightUpdate);
